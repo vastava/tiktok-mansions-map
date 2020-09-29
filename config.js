331b4324fb0902129136718d643d42131b9d050a
Anjali <ashrivastava@berkeley.edu>
2020-09-29 11:36:15 -0700

    add imgs, start storymap

---
+++ config.js
@@ -0,0 +1,50 @@
+var config = {
+	chapters: [
+		{
+			id: 'Overview',
+			title: 'Overview',
+			description: 'okay hello',
+			location: {
+				center: [-95.8, 36.1],
+				zoom: 8.5,
+				pitch: 0,
+				bearing: 0
+			}
+		},	
+		{
+			// 7 9 9 6 10 2 4 5 7 11
+			id: 'Los-Angeles',
+			title: 'The start of it all',
+			description: 'Los Angeles is undoubtedly the hub of this new crop of content houses. Since mid-March, at least 10 mansions have launched here, bringing about 70 creators to LA and surrounding areas.',
+			location: {
+				center: [-118, 34.1],
+				zoom: 8.5,
+				pitch: 0,
+				bearing: 0
+			}
+		},
+		{
+			id: 'Los-Angeles-2',
+			title: 'V@ult House',
+			description: "Let's look at an example.",
+			location: {
+				center: [-118, 34.1],
+				zoom: 8.5,
+				pitch: 0,
+				bearing: 0
+			}
+		},		
+		{
+			id: 'United-States',
+			title: 'testing2',
+			description: 'whaddup usss',
+			location: {
+				center: [-95.8, 36.1],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			align: "centery"
+		}		
+	]
+}
