b6e9dac4702d0709b36f1d6fb63842b864ed958f
Anjali <ashrivastava@berkeley.edu>
2020-09-30 17:44:39 -0700

    update story, add imgs

--- config.js
+++ config.js
@@ -12,21 +12,21 @@ var config = {
 			}
 		},	
 		{
-			// 7 9 9 6 10 2 4 5 7 11
+			// 7 9 9 6 10 2 4 5 7 11 12 4
 			id: 'Los-Angeles',
 			title: 'The start of it all',
-			description: 'Los Angeles is undoubtedly the hub of this new crop of content houses. Since mid-March, at least 10 mansions have launched here, bringing about 70 creators to LA and surrounding areas.',
+			description: 'Los Angeles is undoubtedly the hub of this new crop of content houses. Since mid-March, more than 10 mansions have launched here, bringing about 85 creators to LA and surrounding areas. <br><br> <i>Hover over a <b>point</b> to get information about each mansion.</i>"',
 			location: {
 				center: [-118, 34.1],
-				zoom: 8.5,
+				zoom: 9,
 				pitch: 0,
 				bearing: 0
-			}
+			},
 		},
 		{
 			id: 'Los-Angeles-2',
-			title: 'V@ult House',
-			description: "Let's look at an example.",
+			title: 'Alpha House',
+			description: "One of the newer houses — the Alpha House — came under fire for <a href='https://twitter.com/defnoodles/status/1293292841196597248'>throwing a party</a> with about 150 people only three days after its launch in August. Two of its members are from LA neighborhoods, but let's zoom out a bit. <br><br> <i>Hover over an <b>image</b> to get information about each member.</i>",
 			location: {
 				center: [-118, 34.1],
 				zoom: 8.5,
@@ -36,8 +36,8 @@ var config = {
 		},		
 		{
 			id: 'United-States',
-			title: 'testing2',
-			description: 'whaddup usss',
+			title: 'Alpha House Members',
+			description: "Seven of the nine members moved to LA from outside of the city, five of whom were out-of-state. <br><br> <i>Hover over an image to get information about each member.</i>",
 			location: {
 				center: [-95.8, 36.1],
 				zoom: 4,
@@ -45,6 +45,179 @@ var config = {
 				bearing: 0
 			},
 			align: "centery"
-		}		
+		},
+		{
+			id: 'United-States-2',
+			title: 'Alpha House Members',
+			description: "A neighbor to the Alpha House <a href='https://twitter.com/defnoodles/status/1293292847538429952/photo/1'>claimed</a> that its members have thrown a party every night since moving in.",
+			location: {
+				center: [-95.8, 36.1],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			align: "centery"
+		},		
+		{
+			id: 'Los-Angeles-3',
+			title: 'V@ult House',
+			description: "The V@ult House is another content mansion that launched back in April. Five of its members are from LA.",
+			location: {
+				center: [-118, 34.1],
+				zoom: 8.5,
+				pitch: 0,
+				bearing: 0
+			}
+		},
+		{
+			id: 'United-States-3',
+			title: 'V@ult House Members',
+			description: "And six of its members are from elsewhere, one of whom moved from abroad.",
+			location: {
+				center: [-95.8, 36.1],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			align: "centery"
+		},
+		{
+			id: 'United-States-4',
+			title: 'V@ult House Members',
+			description: "The ever-changing landscape of TikTok mansions and its members make it especially difficult to map. The 'content house' phenomenon arguably began in 2014 with the O2L mansion, which was formed by a group of friends.",
+			location: {
+				center: [-95.8, 36.1],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			image: "o2l.jpg",
+			align: "centery"
+		},
+		{
+			id: 'United-States-5',
+			title: 'V@ult House Members',
+			description: "Nowadays, the space is dominated by talent agencies, marketing departments and corporate brands. Even the multi-million dollar business Fenty Beauty launched their own house in March. This business model has led to drama, and public squabbles between members of many of these new houses, as oftentimes, the creators have not met before moving in together.",
+			location: {
+				center: [-95.8, 36.1],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			image: "fenty.jpeg",
+			align: "centery"
+		},	
+		{
+			id: 'United-States-6',
+			title: 'V@ult House Members',
+			description: "The V@ult House is run by talent agency Six Degrees of Influence, and its members were carefully curated. Other talent agencies can also poach creators of already existing content mansions — Devyn Winkler of Texas, for example, left the V@ult House to join an all-girls content mansion, the 'Not a Content House'. <br><br><i>Members who have left a mansion are marked by a lower opacity image.</i>",
+			location: {
+				center: [-95.8, 36.1],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			align: "centery"
+		},	
+		{
+			id: 'United-States-7',
+			title: 'Not a Content House',
+			description: "Let's look at the Not a Content House (which has also been criticized for <a href='https://twitter.com/defnoodles/status/1293910476422656001'>throwing</a> <a href='https://twitter.com/defnoodles/status/1302652059871637504'>several</a> <a href='https://twitter.com/defnoodles/status/1302243881707216897'>parties</a>).",
+			location: {
+				center: [-95.8, 40.1],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			align: "centery"
+		},		
+		{
+			id: 'Los-Angeles-4',
+			title: 'Not a Content House',
+			description: "Not a Content House is <a href='https://www.globenewswire.com/news-release/2020/08/12/2077112/0/en/Tongji-Healthcare-Group-Inc-Signs-Agreement-to-Acquire-Premier-Social-Media-Influencer-Firm-The-Clubhouse-with-over-70M-Followers.html'>managed</a> by the West of Hudson Group Inc, or WOHG. WOHG owns a network of four content houses, including the Clubhouse, which was founded by Daisy Keech, who also co-founded the Hype House. <br><br> Members of the four houses frequently collaborate with one another.",
+			location: {
+				center: [-118, 34.1],
+				zoom: 8.5,
+				pitch: 0,
+				bearing: 0
+			}
+		},
+		{
+			id: 'Los-Angeles-5',
+			title: 'Not a Content House',
+			description: "The other houses in the network are Clubhouse Next, which is primarily for younger and up-and-coming influencers, and Clubhouse For the Boys.",
+			location: {
+				center: [-118, 34.1],
+				zoom: 8.5,
+				pitch: 0,
+				bearing: 0
+			}
+		},
+		{
+			id: 'United-States-8',
+			title: 'WOHG Network',
+			description: "Together, the network represents nearly 30 different influencers, all of whom moved from various states...",
+			location: {
+				center: [-95.8, 40.1],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			align: "centery"
+		},	
+		{
+			id: 'World-1',
+			title: 'WOHG Network',
+			description: "...and countries.",
+			location: {
+				center: [-75.8, 15],
+				zoom: 2.5,
+				pitch: 0,
+				bearing: 0
+			},
+			align: "centery"
+		},	
+		{
+			id: 'Arizona-1',
+			title: 'Halo House & The House Nobody Asked For',
+			description: "Some influencers have set up shop outside of Los Angeles. The Halo House, for example, is based in Scottsdale, AZ. And the House Nobody Asked For is based in Las Vegas.",
+			location: {
+				// center: [-108, 33.1],
+				center: [-95.8, 40.1],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			align:"centery"
+		},	
+		{
+			id: 'Europe-1',
+			title: 'Europe',
+			description: "The content house phenomenon is rising overseas, as well. There are two content houses based in England, and one in Ireland. There is also a house based in Spain made up entirely of creators from the UK.",
+			location: {
+				// center: [-108, 33.1],
+				center: [14, 48],
+				zoom: 4,
+				pitch: 0,
+				bearing: 0
+			},
+			// align:"centery"
+		},				
+		{
+			id: 'Los-Angeles-6',
+			title: 'Explore',
+			description: "Now explore for yourself. Click on any of the mansions to see where each of its members are from, and use the navigation controls in the top right corner to pan and zoom over the map. The button in the top left corner will clear your selection.",
+			location: {
+				center: [-26, 34.1],
+				zoom: 2,
+				pitch: 0,
+				bearing: 0
+			}
+		},
+		//Content House phenomenon is catching wind overseas
+		//WOHG's Clubhouse is an established network of four social media content creation houses ("Clubhouse BH", "Clubhouse Next", "Clubhouse FTB", and "Not a Content House") 
+		// West of Hudson Group Inc
+		//now explore for yourself. click on any point and use the clear button in the top corner to start anew.
+
 	]
 }
